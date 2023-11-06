a655565a90871b0bbbf86ba3c51fccccae2fa927
fur1ozz <tomassveveris@gmail.com>
2023-11-06 14:55:14 +0200

    tailwind testing

--- src/components/TasksForProject.jsx
+++ src/components/TasksForProject.jsx
@@ -2,26 +2,26 @@
 function TasksForProject(){
     return(
       <>
-        <div className="flex border-2 border-red-500 w-96">
+        <div className="flex border-2 border-red-500 w-4/5">
             <div>
 
             </div>
             <div>
 
             </div>
-            <div className="main-tasks-out-cont">
-                <div className="to-do-tasks-cont">
-                    <div className="tasks-type-title">TO DO 2</div>
-                    <div className="tasks-cont">
+            <div className="w-full p-5 flex justify-evenly flex-wrap">
+                <div className="w-72 bg-neutral-100	rounded h-72 max-h-full p-2 flex flex-col">
+                    <div className="text-neutral-500 font-medium pt-1 pb-2">TO DO 2</div>
+                    <div className="tasks-cont bg-white p-3 rounded h-52">
                         <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
                     </div>
                 </div>
-                <div className="in-progress-tasks-cont">
-                    <div className="tasks-type-title">IN PROGRESS 0</div>
+                <div className="w-72 bg-neutral-100	rounded h-72 max-h-full p-2 flex flex-col">
+                    <div className="text-neutral-500 font-medium pt-1 pb-2">IN PROGRESS 0</div>
 
                 </div>
-                <div className="done-tasks-cont">
-                    <div className="tasks-type-title">DONE 0</div>
+                <div className="w-72 bg-neutral-100	rounded h-72 max-h-full p-2 flex flex-col">
+                    <div className="text-neutral-500 font-medium pt-1 pb-2">DONE 0</div>
 
                 </div>
             </div>
