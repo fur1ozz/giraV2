03b98a5aa3b4192e9ac69c89aa380a4ffe22617e
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-11-16 20:37:25 +0200

    adjusted app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProjectPage from "./pages/ProjectPage";
 import CertainProjectCalendarPage from "./pages/CertainProjectCalendarPage";
 import ProjectCalendarPickerPage from "./pages/ProjectCalendarPickerPage";
@@ -7,27 +7,15 @@ import "./styles/tailwind.css";
 import Register from "./components/Register";
 
 function App() {
-  const isAuthenticated = localStorage.getItem('token');
   return (
     <Router>
       <div className="min-h-screen dark:bg-[#1d2125]">
         <Routes>
+          <Route exact path="/" />
+          <Route path="/Project" element={<ProjectPage />} />
+          <Route path="/CertainProjectCalendar/:projectId" element={<CertainProjectCalendarPage />} />
+          <Route path="/ProjectCalendarPicker" element={<ProjectCalendarPickerPage />} />
           <Route path="/Register" element = {<Register />}/>
-          {isAuthenticated ? (
-            <>
-            <Route exact path="/" />
-            <Route path="/Project" element={<ProjectPage />} />
-            <Route path="/CertainProjectCalendar/:projectId" element={<CertainProjectCalendarPage />} />
-            <Route path="/ProjectCalendarPicker" element={<ProjectCalendarPickerPage />} />
-            </>
-          ) : (
-            <>
-            <Route index element={<Navigate to="/Register" />} /> 
-            <Route path="/Project" element={<ProjectPage />} />
-            <Route path="/CertainProjectCalendar/:projectId" element={<CertainProjectCalendarPage />} />
-            <Route path="/ProjectCalendarPicker" element={<ProjectCalendarPickerPage />} />
-            </>
-          )};
         </Routes>
       </div>
     </Router>
