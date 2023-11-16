d927c4a19e12cd64a406c50f4b4fd1f9252e5f62
fur1ozz <tomassveveris@gmail.com>
2023-11-16 19:23:46 +0200

    created Project page and added styling based on month

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProjectPage from "./pages/ProjectPage";
 import "./styles/tailwind.css";
+import Projects from "./components/Projects";
 // import {useEffect, useState} from "react";
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
           <div className="min-h-screen dark:bg-[#1d2125]">
             <Routes>
               <Route exact path="/" />
-              <Route path="/project" element={<ProjectPage />}/>
+              <Route path="/project/task" element={<ProjectPage />}/>
+              <Route path="/project" element={<Projects />}/>
             </Routes>
           </div>
         </Router>
